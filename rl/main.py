0730762a2e1bd7d65f1379461a8fef1982c1b7e9
Pradipta Bora <190050089@iitb.ac.in>
2022-05-04 23:24:46 +0530

    add stable baselines3 agent boilerplate

--- rl/main.py
+++ rl/main.py
@@ -1,6 +1,7 @@
 from env import SchedulingEnv
 import gym
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3 import A2C, DDPG, DQN, TD3
 import argparse
 
 def parse_arguments():
@@ -11,18 +12,37 @@ def parse_arguments():
                     help="burst1:arrivalTime1,burst2:arrivalTime2, ...")
     ap.add_argument("-b", "--boost", default=0,
                     help="", type=int)
+    ap.add_argument("-a", "--agent", default="DQN", help="Agent", type=str)
     return ap
 
 arg_parser = parse_arguments()
 args = vars(arg_parser.parse_args())
 env = SchedulingEnv(args)
 
-st = env.reset()
-while True:
-    st, r, done, _ = env.step(st)
-    if done:
-        break
+agent_dict = {"A2C": A2C, "DQN":DQN, "DDPG":DDPG, "TD3":TD3}
 
-env.render()
-
+model = agent_dict[args.get("agent", "DQN")]('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=100000)
+# bound on iterations = 100000
 check_env(env, warn=True)
+
+obs = env.reset()
+r = 0
+for i in range(100000):
+    action, _state = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    r += reward
+    if done:
+      obs = env.reset()
+      break 
+
+print ("reward = ", r)
+# st = env.reset()
+# while True:
+#     st, r, done, _ = env.step(st)
+#     if done:
+#         break
+
+# env.render()
+
